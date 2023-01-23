bfdd1b913e90bab740b7265eb4390e9117541d86
Pooja <studentpooja1234@gmail.com>
2023-01-23 14:32:51 +0530

    Started

---
+++ skygroup_chatapp/lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants{
+  static String appId ="1:514582539073:web:273713f2a8e8798f380fc0";
+  static String apiKey="AIzaSyArqD28E4aPhELFeAJtXhN5y4_873Kn0mQ";
+  static String messagingSenderId="514582539073";
+  static String projectId="skygroup-chatapp";
+
+}
